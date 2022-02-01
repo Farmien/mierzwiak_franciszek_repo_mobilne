5c8c40e0b03701916e65b1afd326b22f0b77e40c
Farmien <franula98765@gmail.com>
2022-02-01 15:58:44 +0100

    01.02.2022 - ostatni ostatniego komita

--- Pozycjonownie_Elementow_09/app/src/main/java/com/example/pozycjonownie_elementow_09/MainActivity.java
+++ Pozycjonownie_Elementow_09/app/src/main/java/com/example/pozycjonownie_elementow_09/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pozycjonownie_elementow_09;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button buttonSave = findViewById(R.id.ButtonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
     }
+
 
 
 }

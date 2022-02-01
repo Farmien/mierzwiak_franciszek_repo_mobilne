3feaf97644436a32c13adfa0d8be99176d8856e7
Farmien <franula98765@gmail.com>
2022-02-01 15:41:06 +0100

    01.02.2022 - cummit typu ostatniego

--- Pozycjonownie_Elementow_09/app/src/main/java/com/example/pozycjonownie_elementow_09/MainActivity.java
+++ Pozycjonownie_Elementow_09/app/src/main/java/com/example/pozycjonownie_elementow_09/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pozycjonownie_elementow_09;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonSave = findViewById(R.id.ButtonSave);
     }
+
+
 }
